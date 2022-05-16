5f600fdad85da5244061270dff0a6b8da6aa6805
Krishna-Rani-t <105223454+Krishna-Rani-t@users.noreply.github.com>
2022-05-16 08:11:38 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,17 @@
 #include "stats.h"
 
-float Statistics::ComputeStatistics(const std::vector<___>& ) {
+void Statistics::ComputeStatistics(const std::vector<float>& v) {
     //Implement statistics here
-}
+	average = accumulate( v.begin(), v.end(), 0.0/ v.size());
+	 int min = INT_MAX, max = INT_MIN;
+	    for (int i: v)
+	    {
+	        if (i < min) {
+	            min = i;
+	        }
+
+	        if (i > max) {
+	            max = i;
+	        }
+	    }
+
