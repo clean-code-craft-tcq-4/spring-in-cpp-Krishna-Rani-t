8040b298a86379bd7157a006206a1d6b40207d05
Krishna-Rani-t <105223454+Krishna-Rani-t@users.noreply.github.com>
2022-05-19 14:33:57 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -18,33 +18,3 @@ void Statistics::ComputeStatistics(const std::vector<float>& v) {
 	    }
 	    average = sum/v.size();
 }
-void IAlerter::setEmailSent(bool flag)
-{
-     emailSent += flag;
-}
-void IAlerter::setLedAlert(bool flag)
-{
-    ledGlows += flag;
-}
-void StatsAlerter::checkAndAlert(std::vector<float> inputData)
-    {
-        for(auto data:inputData)
-        {
-            if(data > m_MaxThreshold)
-            {
-                if(!m_Alerter.empty())
-                {
-                    m_Alerter[0]->setEmailSent(true);
-                    m_Alerter[1]->setLedAlert(true);
-                }
-            }
-            else
-            {
-                if(!m_Alerter.empty())
-                {
-                    m_Alerter[0]->setEmailSent(false);
-                    m_Alerter[1]->setLedAlert(false);
-                }
-            }
-        }
-    } 
