ce53d37a1378abc4eff5c5f31c0c4bc48273ed81
Krishna-Rani-t <105223454+Krishna-Rani-t@users.noreply.github.com>
2022-05-19 14:35:03 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -8,32 +8,4 @@ public:
        static void ComputeStatistics(const std::vector<float>& v );
 };
 
-class IAlerter
-{  
-  public:
-    bool emailSent;
-    bool ledGlows;
-    void setEmailSent(bool flag);
-    void setLedAlert(bool flag);
-};
 
-class EmailAlert:public IAlerter
-{
-       
-};
-
-class LEDAlert:public IAlerter
-{
-   
-};
-
-class StatsAlerter
-{
-  const float m_MaxThreshold;
-  std::vector<IAlerter*> m_Alerter;
-public:
-    StatsAlerter(const float maxThreshold,std::vector<IAlerter*> alerter):m_MaxThreshold(maxThreshold),m_Alerter(alerter)
-    {
-    }
-    void checkAndAlert(std::vector<float> inputData);
-};
